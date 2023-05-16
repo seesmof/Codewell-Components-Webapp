63c36a42ea305ab3c9bc5ea0a3c74fe8ed4917e3
Oleh Onyshchenko <seesmwork@gmail.com>
2023-05-16 09:05:19 +0300

    feat(snipper-landing-page): add new site images

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
