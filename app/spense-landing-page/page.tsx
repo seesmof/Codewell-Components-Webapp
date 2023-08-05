4656cd0402c8a289c4c82d7f3914a41da3cc2ce7
Oleh Onyshchenko <seesmwork@gmail.com>
2023-08-05 13:41:34 +0000

    🌿: Saturday, August 5, 2023 at 4:41:34 PM Eastern European Summer Time

--- app/spense-landing-page/page.tsx
+++ app/spense-landing-page/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { BsArrowRight } from "react-icons/bs";
 
 const MainPage = () => {
   return (
     <>
-      <section className="w-full p-4 bg-black">
+      <section className="w-full p-4 px-6 bg-black">
         <div className="font-medium text-center text-white">
           🎉 To celebrate Spense's launch, we will be waiving all fees for the
           entire month of April.
@@ -43,32 +44,32 @@ const MainPage = () => {
 
           <a href="#" className="sm:hidden">
             <img
-              src="/spense-landing-page/Hamburger Menu.svg"
+              src="/spense-landing-page/Assets/Hamburger Menu.svg"
               alt="Hamburger menu icon"
             />
           </a>
         </header>
 
-        <main className="lg:flex-row lg:gap-24 md:items-center max-w-7xl container flex flex-col pt-6 mx-auto">
+        <main className="lg:flex-row lg:gap-24 md:items-center max-w-7xl container flex flex-col py-6 mx-auto">
           <img
-            src="/spense-landing-page/Hero Image (Mobile).png"
+            src="/spense-landing-page/Assets/Hero Image (Mobile).png"
             alt="image of monthly earnings"
             className="sm:hidden object-cover"
           />
           <img
-            src="/spense-landing-page/Hero Image (Tablet).png"
+            src="/spense-landing-page/Assets/Hero Image (Tablet).png"
             alt="image of monthly earnings"
             className="sm:block lg:hidden hidden object-cover"
           />
           <div className="lg:max-w-sm lg:order-2">
             <img
-              src="/spense-landing-page/Hero Image (Desktop).png"
+              src="/spense-landing-page/Assets/Hero Image (Desktop).png"
               alt="image of monthly earnings"
               className="lg:block hidden object-cover"
             />
           </div>
 
-          <div className="sm:pt-8 lg:w-2/3 lg:order-1 flex flex-col pt-6">
+          <div className="sm:pt-8 lg:w-2/3 lg:order-1 flex flex-col space-y-2 pt-6">
             <h1 className="sm:text-4xl lg:text-6xl lg:max-w-full md:text-5xl sm:max-w-md text-2xl font-bold">
               Share your unfiltered thoughts. Get paid.
             </h1>
@@ -80,32 +81,28 @@ const MainPage = () => {
             </p>
             <div className="sm:pt-4 text-stone-600 flex flex-col gap-2 pt-3 font-medium">
               <div className="flex flex-row items-center gap-3">
-                <img src="/spense-landing-page/Checkmark.svg" alt="" />
+                <img src="/spense-landing-page/Assets/Checkmark.svg" alt="" />
                 Receive 99% of the earnings.
               </div>
               <div className="flex flex-row items-center gap-3">
-                <img src="/spense-landing-page/Checkmark.svg" alt="" />
+                <img src="/spense-landing-page/Assets/Checkmark.svg" alt="" />
                 Get paid via Debit Card, ACH, or PayPal
               </div>
               <div className="flex flex-row items-center gap-3">
-                <img src="/spense-landing-page/Checkmark.svg" alt="" />
+                <img src="/spense-landing-page/Assets/Checkmark.svg" alt="" />
                 Withdraw your earnings anytime.
               </div>
             </div>
 
-            <div className="flex flex-row gap-3 pt-5">
+            <div className="flex flex-row items-center pt-4 gap-2">
               <input
                 type="email"
-                className="rounded px-4 py-2 sm:py-3 focus:outline-0 focus:outline-none focus:ring-gray-600 focus:ring-1 border-gray-400 focus:border-gray-600 max-w-[11rem]"
-                name="email"
                 placeholder="john@example.com"
+                className="px-4 py-3 rounded-md border-neutral-400 border"
               />
-              <a
-                href="#"
-                className="bg-black text-white font-medium rounded px-4 py-2 hover:bg-neutral-800 duration-300 sm:py-3 sm:px-5"
-              >
+              <button className="bg-black rounded-md px-4 py-3 text-white">
                 Get Started
-              </a>
+              </button>
             </div>
           </div>
         </main>
@@ -116,21 +113,21 @@ const MainPage = () => {
           <section className="grid items-center justify-center max-w-full grid-cols-3 gap-4 my-8 mb-12">
             <div className="col-span-1">
               <img
-                src="/spense-landing-page/Facebook Logo.png"
+                src="/spense-landing-page/Assets/Facebook Logo.png"
                 alt=""
                 className=""
               />
             </div>
             <div className="col-span-1">
               <img
-                src="/spense-landing-page/Dribbble Logo.png"
+                src="/spense-landing-page/Assets/Dribbble Logo.png"
                 alt=""
                 className=""
               />
             </div>
             <div className="col-span-1">
               <img
-                src="/spense-landing-page/Youtube Logo.png"
+                src="/spense-landing-page/Assets/Youtube Logo.png"
                 alt=""
                 className=""
               />
@@ -151,20 +148,19 @@ const MainPage = () => {
                 href="#"
                 className="text-stone-600 underline-offset-2 hover:text-stone-700 flex items-center gap-1 pt-3 text-lg font-medium underline duration-300"
               >
-                Learn more about Escrow
-                <i className="bi bi-arrow-right text-sm"></i>
+                Learn more about Escrow <BsArrowRight />
               </a>
             </div>
             <img
-              src="/spense-landing-page/Phone Mockup (Mobile).png"
+              src="/spense-landing-page/Assets/Phone Mockup (Mobile).png"
               alt=""
               className="w-full"
             />
           </section>
 
-          <section className="flex flex-col py-6">
-            <h2 className="text-4xl font-bold">A text editor like no other.</h2>
-            <p className="sm:pt-4 text-stone-600 pt-3 text-lg">
+          <section className="flex flex-col py-12">
+            <h2 className="text-3xl font-bold">A text editor like no other.</h2>
+            <p className="sm:pt-4 text-stone-600 pt-3">
               Our text editor pulls you into focus mode with it's simplistic
               design and usability so you can put out your best writings.
             </p>
@@ -172,11 +168,10 @@ const MainPage = () => {
               href="#"
               className="text-stone-600 underline-offset-2 hover:text-stone-700 flex items-center gap-2 pt-3 text-lg font-medium tracking-wide underline capitalize duration-300"
             >
-              text editor live demo
-              <i className="bi bi-arrow-right text-sm"></i>
+              text editor live demo <BsArrowRight />
             </a>
             <img
-              src="/spense-landing-page/Text Editor.png"
+              src="/spense-landing-page/Assets/Text Editor.png"
               alt=""
               className="w-full pt-8 scale-105"
             />
@@ -184,79 +179,37 @@ const MainPage = () => {
         </div>
       </section>
 
-      <footer className="md:p-6 p-4 text-white bg-black">
-        <div className="max-w-7xl container flex flex-col items-start justify-between py-8 mx-auto">
-          <section className="flex flex-col gap-2">
-            <h2 className="hover:text-neutral-200 text-lg font-medium duration-300">
-              spense.
-            </h2>
-            <p className="text-neutral-400">
-              Spense is an open platform for individuals to share their
-              unfiltered thoughts. Discuss the topic you love, and get paid for
-              doing
-              <span className="italic">just</span> that.
-            </p>
-          </section>
+      <footer className="flex flex-col bg-black text-white/60 py-12 px-4 lg:px-6">
+        <FooterSection heading="spense.">
+          <p>
+            Spense is an open platform for individuals to share their unfiltered
+            thoughts. Discuss the topics you love, and get paid for doing{" "}
+            <em className="italic pr-1">just</em>
+            that.
+          </p>
+        </FooterSection>
 
-          <section className="gap-y-8 grid justify-between w-full grid-cols-2 pt-8">
-            <article className="flex flex-col col-span-1 gap-3">
-              <h2 className="hover:text-neutral-200 text-lg font-medium capitalize duration-300">
-                company
-              </h2>
-              <div className="text-neutral-400 flex flex-col gap-2 capitalize">
-                <a href="#" className="hover:text-neutral-200 duration-300">
-                  about
-                </a>
-                <a href="#" className="hover:text-neutral-200 duration-300">
-                  customers
-                </a>
-                <a href="#" className="hover:text-neutral-200 duration-300">
-                  blog
-                </a>
-              </div>
-            </article>
-
-            <article className="flex flex-col col-span-1 gap-3">
-              <h2 className="hover:text-neutral-200 text-lg font-medium capitalize duration-300">
-                opportunities
-              </h2>
-              <div className="text-neutral-400 flex flex-col gap-2 capitalize">
-                <a href="#" className="hover:text-neutral-200 duration-300">
-                  jobs
-                </a>
-              </div>
-            </article>
-
-            <article className="flex flex-col col-span-1 gap-3">
-              <h2 className="hover:text-neutral-200 text-lg font-medium capitalize duration-300">
-                sitemap
-              </h2>
-              <div className="text-neutral-400 flex flex-col gap-2 capitalize">
-                <a href="#" className="hover:text-neutral-200 duration-300">
-                  homepage
-                </a>
-              </div>
-            </article>
-
-            <article className="flex flex-col col-span-1 gap-3">
-              <h2 className="hover:text-neutral-200 text-lg font-medium capitalize duration-300">
-                resources
-              </h2>
-              <div className="text-neutral-400 flex flex-col gap-2 capitalize">
-                <a href="#" className="hover:text-neutral-200 duration-300">
-                  support
-                </a>
-                <a href="#" className="hover:text-neutral-200 duration-300">
-                  contact
-                </a>
-                <a href="#" className="hover:text-neutral-200 duration-300">
-                  FAQ
-                </a>
-              </div>
-            </article>
-          </section>
+        <div className="grid grid-cols-2 grid-rows-2">
+          <div className="flex flex-col"></div>
         </div>
       </footer>
+    </>
+  );
+};
+
+const FooterSection = ({
+  heading,
+  children,
+}: {
+  heading: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <>
+      <div className="flex flex-col gap-3">
+        <strong className="font-medium text-white">{heading}</strong>
+        {children}
+      </div>
     </>
   );
 };
